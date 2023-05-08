31a3aa4f5eca3f295b4e1f53f394e9047e2c1ffc
Kyialbek <28112002akn@gmail.com>
2023-05-08 20:25:41 +0600

    sayHello

--- src/main/java/org/example/config/Hello.java
+++ src/main/java/org/example/config/Hello.java
@@ -1,2 +1,7 @@
-package org.example.config;public class Hello {
+package org.example.config;
+
+public class Hello {
+    static void sayHello(String zero) {
+        System.out.println(zero.hashCode());
+    }
 }
